e503e0e626dcf6586854a397947ae532191feb77
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-02-14 21:17:39 -0500

    ejercicio 4 resuelto

---
+++ Ejercicio4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Ingrese el radio del círculo: ");
+        double radio = scanner.nextDouble();
+        
+        
+        double area = Math.PI * Math.pow(radio, 2);
+        
+        
+        System.out.println("El área del círculo es: " + area);
+        
+        scanner.close();
+    }
+}
+
