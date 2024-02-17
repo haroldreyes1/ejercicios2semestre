df788b0a49ec6ff74b01da65509437b537de1710
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-02-17 17:28:50 -0500

    ejercicio 18 resuelto

---
+++ Ejercicio18.java
@@ -0,0 +1,8 @@
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        int num = 125;
+        int cuadrado = num * num;
+        System.out.println("El cuadrado de " + num + " es: " + cuadrado);
+    }
+}
+
