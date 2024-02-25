35bccd40d1b233a2ced2f63e48765a3c3a4a8e05
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-02-25 16:01:46 -0500

    ejercicio 31 resuelto

---
+++ Ejercicio31.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la base del triángulo en milímetros:");
+        double base = scanner.nextDouble();
+
+        System.out.println("Ingrese la altura del triángulo en milímetros:");
+        double altura = scanner.nextDouble();
+
+        double areaEnMilimetrosCuadrados = (base * altura) / 2;
+        double areaEnCentimetrosCuadrados = areaEnMilimetrosCuadrados / 100;
+        double areaEnMetrosCuadrados = areaEnCentimetrosCuadrados / 10000; 
+
+        System.out.println("El área del triángulo es:");
+        System.out.println("En milímetros cuadrados: " + areaEnMilimetrosCuadrados + " mm^2");
+        System.out.println("En centímetros cuadrados: " + areaEnCentimetrosCuadrados + " cm^2");
+        System.out.println("En metros cuadrados: " + areaEnMetrosCuadrados + " m^2");
+
+        scanner.close();
+    }
+}
+
