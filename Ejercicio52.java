3dee488a44b81da83a7eaf7c47c14f57f1f52676
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-03-12 21:40:38 -0500

    ejercicio 52 resuelto

--- Ejercicio52.java
+++ Ejercicio52.java
@@ -1,6 +1,4 @@
-//Crear una función que reciba como parámetro un número entero y escriba la tabla
-//de multiplicar de ese número (por ejemplo: para el 3 deberá llegar desde 3x0=0
-//hasta 3x10=
+
 public class Ejercicio52 {
     public static void main(String[] args) {
         int numero = 3;
