c8f0441259ae45628f4642f9cec7e85271bbb31f
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-03-12 21:46:06 -0500

    ejercicio 45 resuelto

--- Ejercicio45.java
+++ Ejercicio45.java
@@ -1,5 +1,4 @@
-//Crear un función que reciba un número real y devuelva un número entero con el
-//valor: -1 si el número es negativo, 1 si el número es positivo o 0 si es cero.
+
 public class Ejercicio45 {
     public static void main(String[] args) {
         double numero = -3.5;
