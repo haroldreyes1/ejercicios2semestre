d049041ab93aa8c0f79b97472143497f598020ea
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-03-17 18:23:38 -0500

    ejercicio 70 resuelto

---
+++ Ejercicio70.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio70 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el valor de n: ");
+        int n = scanner.nextInt();
+
+        int resultado = calcularResultado(n);
+        System.out.println("El resultado de la expresión es: " + resultado);
+
+        scanner.close();
+    }
+
+    public static int calcularResultado(int n) {
+        int resultado = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                resultado -= i; 
+            } else {
+                resultado += i; 
+            }
+        }
+        return resultado;
+    }
+}
+
