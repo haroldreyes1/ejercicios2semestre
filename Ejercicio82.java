7321520aca369ec0ebbf219fc7059aa6ab61ed33
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-06 21:41:04 -0500

    ejercicio 82 resuelto

---
+++ Ejercicio82.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio82 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        double numero = scanner.nextDouble();
+
+        if (numero > 0) {
+            System.out.println("El número ingresado es positivo.");
+        } else if (numero < 0) {
+            System.out.println("El número ingresado es negativo.");
+        } else {
+            System.out.println("El número ingresado es cero.");
+        }
+
+        scanner.close();
+    }
+}
+
