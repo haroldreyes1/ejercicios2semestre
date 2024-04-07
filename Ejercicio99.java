93900105174dcb0e423a83d7c8a66141939e681c
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-06 22:33:42 -0500

    ejercicio 99 resuelto

---
+++ Ejercicio99.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio99 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Ingrese un año: ");
+        int año = scanner.nextInt();
+
+        
+        if (esBisiesto(año)) {
+            System.out.println(año + " es un año bisiesto.");
+        } else {
+            System.out.println(año + " no es un año bisiesto.");
+        }
+
+        scanner.close();
+    }
+
+    
+    public static boolean esBisiesto(int año) {
+        return (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+    }
+}
+
