34d9ccd627353876e59c73b4ec16d31432cd096e
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-06 21:48:30 -0500

    ejercicio 85 resuelto

---
+++ Ejercicio85.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio85 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número ingresado es par.");
+        } else {
+            System.out.println("El número ingresado es impar.");
+        }
+
+        scanner.close();
+    }
+}
+
