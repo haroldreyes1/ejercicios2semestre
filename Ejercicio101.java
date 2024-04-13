16914f63fa2b69cdc2743cee7d5eb5be2eb8f45b
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-13 11:21:50 -0500

    ejercicio 101 resuelto

---
+++ Ejercicio101.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio101 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número entero:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Ingrese el segundo número entero:");
+        int num2 = scanner.nextInt();
+
+        
+        if (num1 % num2 == 0) {
+            System.out.println(num1 + " es divisible por " + num2);
+        } else {
+            System.out.println(num1 + " no es divisible por " + num2);
+        }
+
+        
+        scanner.close();
+    }
+}
+
