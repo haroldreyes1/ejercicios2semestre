a8acaa90cfa26d6f54b6681de93c0eb9827736c1
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-13 11:38:42 -0500

    ejercicio 108 resuelto

---
+++ Ejercicio108.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Ingrese un número:");
+        int numero = scanner.nextInt();
+
+        
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+
+        
+        scanner.close();
+    }
+}
+
