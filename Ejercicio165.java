d35671569a754b35ef8b4ace5215672c151739f1
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-15 18:11:55 -0500

    ejercicio 165 resuelto

---
+++ Ejercicio165.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejercicio165 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Ingrese la cantidad de horas: ");
+        int horas = scanner.nextInt();
+        System.out.print("Ingrese la cantidad de minutos: ");
+        int minutos = scanner.nextInt();
+        System.out.print("Ingrese la cantidad de segundos: ");
+        int segundos = scanner.nextInt();
+
+        
+        int totalSegundos = horas * 3600 + minutos * 60 + segundos;
+
+        
+        System.out.println("El total de segundos es: " + totalSegundos);
+    }
+}
+
