9f949c5293aaf1362133e2ee12912d17ec8b92f9
HaroldReyes01 <harold.hoyreyes@unicolombo.edu.co>
2024-04-23 19:04:48 -0500

    ejercicio 217 resuelto

---
+++ Ejercicio217.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio217 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 9; i++) {
+            
+            int cuadrado = i * i;
+            
+            System.out.println("El cuadrado de " + i + " es: " + cuadrado);
+        }
+    }
+}
+
